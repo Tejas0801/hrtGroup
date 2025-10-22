7ec945f7c308ffe2da15054f6605e91ff65ed909
Tejas0801 <tejakandhikatla97@gmail.com>
2025-10-22 15:46:05 +0530

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+// Vite provides BASE_URL ("/" in dev, "/hrtGroup/" in production/build when base is set)
+const basename = import.meta.env.BASE_URL ?? "/";
+
 const rootElement = document.getElementById("root")!;
 const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/hrtGroup/">
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
